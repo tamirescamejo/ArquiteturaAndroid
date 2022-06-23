bfd15d1667fe70f43ade63baf6ce848a2508b187
FepaZup <felipe.dearaujo@zup.com.br>
2022-06-22 22:38:31 -0300

    set up home activity

--- MovieFlix/app/src/main/java/br/com/zup/movieflix/home/view/HomeActivity.kt
+++ MovieFlix/app/src/main/java/br/com/zup/movieflix/home/view/HomeActivity.kt
@@ -3,8 +3,10 @@ package br.com.zup.movieflix.home.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
-import br.com.zup.movieflix.R
+import androidx.recyclerview.widget.LinearLayoutManager
 import br.com.zup.movieflix.databinding.ActivityHomeBinding
+import br.com.zup.movieflix.home.model.Movie
+import br.com.zup.movieflix.home.view.adapter.HomeAdapter
 import br.com.zup.movieflix.home.viewmodel.HomeViewModel
 
 class HomeActivity : AppCompatActivity() {
@@ -13,6 +15,9 @@ class HomeActivity : AppCompatActivity() {
         ViewModelProvider(this)[HomeViewModel::class.java]
     }
 
+    private val adapter: HomeAdapter by lazy {
+        HomeAdapter(arrayListOf(), this::goToMovieDetail)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
@@ -20,11 +25,23 @@ class HomeActivity : AppCompatActivity() {
 
         viewModel.getAllMovies()
         observables()
+        setUpRvMovieList()
     }
 
     private fun observables(){
         viewModel.response.observe(this) {
-            binding.tvMovies.text = "$it"
+            adapter.atualizarListaFilme(it)
         }
     }
+
+    private fun setUpRvMovieList(){
+        binding.rvMovieList.adapter = adapter
+        binding.rvMovieList.layoutManager = LinearLayoutManager(this)
+    }
+
+
+    private fun goToMovieDetail(movie: Movie) {
+        //TODO ir para a tela de detalhes do flme
+    }
+
 }
--- MovieFlix/app/src/main/java/br/com/zup/movieflix/login/repository/LoginRepository.kt
+++ MovieFlix/app/src/main/java/br/com/zup/movieflix/login/repository/LoginRepository.kt
@@ -5,11 +5,8 @@ import br.com.zup.movieflix.login.model.LoginModel
 class LoginRepository {
     fun authenticate (login : LoginModel) :LoginModel{
 
-        if(login.user == "usuario" && login.password == "1234"){
-            login.accessAuth = true
-        }else{
-            login.accessAuth = false
-        }
+        login.accessAuth = (login.user == "usuario" && login.password == "1234") ||
+                (login.user == "" && login.password == "")
 
         return login
     }
